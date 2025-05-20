d316c2bf9cb25c69e634462d6d9d2ea83fb14688
Jessica Sousa <jessica.persou@gmail.com>
2025-05-20 01:55:28 -0300

    add configs

---
+++ aura-ms-order/src/main/java/com/postech/auramsorder/config/DataSourceConfig.java
@@ -0,0 +1,63 @@
+package com.postech.auramsorder.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean(name = "orderDataSource")
+    @Primary
+    public DataSource orderDataSource() {
+        return DataSourceBuilder.create()
+                .url("jdbc:postgresql://localhost:5432/AURA_MS_ORDER")
+                .username("postgres")
+                .password("postgres")
+                .driverClassName("org.postgresql.Driver")
+                .build();
+    }
+
+    @Bean(name = "clientDataSource")
+    public DataSource clientDataSource() {
+        return DataSourceBuilder.create()
+                .url("jdbc:postgresql://localhost:5434/aura_ms_client")
+                .username("postgres")
+                .password("postgres")
+                .driverClassName("org.postgresql.Driver")
+                .build();
+    }
+
+    @Bean(name = "productDataSource")
+    public DataSource productDataSource() {
+        return DataSourceBuilder.create()
+                .url("jdbc:postgresql://localhost:5433/aura_ms_product")
+                .username("postgres")
+                .password("postgres")
+                .driverClassName("org.postgresql.Driver")
+                .build();
+    }
+
+    @Bean(name = "stockDataSource")
+    public DataSource stockDataSource() {
+        return DataSourceBuilder.create()
+                .url("jdbc:postgresql://localhost:5435/aura_ms_stock")
+                .username("postgres")
+                .password("postgres")
+                .driverClassName("org.postgresql.Driver")
+                .build();
+    }
+
+    @Bean(name = "paymentDataSource")
+    public DataSource paymentDataSource() {
+        return DataSourceBuilder.create()
+                .url("jdbc:postgresql://localhost:5436/aura_ms_payment")
+                .username("postgres")
+                .password("postgres")
+                .driverClassName("org.postgresql.Driver")
+                .build();
+    }
+}
